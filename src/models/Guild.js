23a07d642cbf9bae908af33f639533baa413b167
!  va1les <67857820+va1les@users.noreply.github.com>
2025-02-03 23:44:10 +0500

    The old code was deleted

--- src/models/Guild.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const guildSchema = mongoose.Schema({
-    guildId: String,
-})
-
-module.exports = mongoose.model('guild', guildSchema);
--- src/models/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = mongoose.Schema({
-    guildId: String,
-    userId: String,
-})
-
-module.exports = mongoose.model('user', userSchema);
